7f3fcc643d108a001b2e18e7c5aee23d027b1e4c
Umayantha93 <umayantha93@gmail.com>
2022-03-28 12:03:02 +0530

    create imgrations and models

---
+++ database/migrations/2022_03_28_063049_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->unsignedBigInteger('company');
+            $table->string('email');
+            $table->bigInteger('phone');
+            $table->timestamps();
+            $table->foreign('company')->references('id')->on('companies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
